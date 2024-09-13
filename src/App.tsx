12f3657a1ac1a2d32f10732cde6c11c8e137622f
saiffardin <saiffardin@gmail.com>
2024-09-14 03:47:12 +0600

    skeleton ready

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, Text } from 'react-native';
+import { styles } from './styles';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text style={styles.containerTxt}>
+          Helllllllllllllllllllllllllloooooooooooo
+        </Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
